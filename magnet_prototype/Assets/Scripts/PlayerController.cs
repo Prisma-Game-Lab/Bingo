e21cd6b04937fad03f85ffa070bce70a7e6eb4c6
rafaelrc7 <rafaelrc7@gmail.com>
2022-03-13 23:39:53 -0300

    Player Controller initial implementation

---
+++ magnet_prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MagnetGame
+{
+	public class PlayerController : MonoBehaviour
+	{
+		private List<Magnet> magnets = new List<Magnet>();
+		private List<Magnet> hand = new List<Magnet>();
+
+		public List<Magnet> Magnets { get => magnets; }
+		public List<Magnet> Hand { get => hand; }
+
+		public void AddToMagnets(List<Magnet> magnets) => this.magnets.AddRange(magnets);
+		public void AddToMagnets(Magnet magnet) => magnets.Add(magnet);
+
+		public Magnet HandRemoveAt(int index) {
+			Magnet magnet = hand[index];
+			hand.RemoveAt(index);
+			return magnet;
+		}
+
+		public void AddToHand(Magnet magnet) => this.magnets.Add(magnet);
+		public void AddToHand(List<Magnet> magnets) => this.magnets.AddRange(magnets);
+	}
+}
+
