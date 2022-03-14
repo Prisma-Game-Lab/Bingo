b2a1ea64f120d50ddd07351e998c4bc286db92f8
rafaelrc7 <rafaelrc7@gmail.com>
2022-03-13 23:15:16 -0300

    Implemented Magnet Pile

---
+++ magnet_prototype/Assets/Scripts/GameObjects/MagnetPile.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MagnetGame
+{
+	public class MagnetPile : MonoBehaviour
+	{
+		private List<Magnet> magnets = new List<Magnet>();
+
+		public List<Magnet> Magnets { get; }
+
+		public void InsertMagnet(Magnet magnet) => magnets.Add(magnet);
+
+		public Magnet getMagnet() {
+			int index = Random.Range(0, magnets.Count);
+			Magnet magnet = magnets[index];
+			magnets.RemoveAt(index);
+			return magnet;
+		}
+	}
+}
+
