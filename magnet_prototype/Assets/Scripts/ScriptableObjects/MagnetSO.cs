f366332c1fcb0910d848faca68de5ce23553a3f1
rafaelrc7 <rafaelrc7@gmail.com>
2022-03-15 19:26:42 -0300

    Implemented Magnet SO

---
+++ magnet_prototype/Assets/Scripts/ScriptableObjects/MagnetSO.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace MagnetGame
+{
+	[CreateAssetMenu(fileName = "Magnet", menuName = "ScriptableObjects/MagnetScriptableObject")]
+	public class MagnetSO : ScriptableObject
+	{
+		public string magnet_name;
+		public string magnet_description;
+		public Type magnet_type;
+		// TODO: magnet effect
+	}
+}
+
